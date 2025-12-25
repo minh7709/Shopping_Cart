f860a42a3a64a2883847f20547eeca49575ae8f6
minh7709 <minh2k32003@gmail.com>
2025-12-25 16:29:47 +0700

    First init spring project and connect database

---
+++ src/main/java/ecomerce/entity/Cart.java
@@ -0,0 +1,16 @@
+package ecomerce.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "carts")
+@Data
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
---
+++ src/test/java/ecomerce/EComerceApplicationTests.java
@@ -0,0 +1,13 @@
+package ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
